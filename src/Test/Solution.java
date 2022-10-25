ad161d942bc54c8fe9b8a7a7ca1664538fd11279
최수원 <suwon3274@gmail.com>
2022-10-25 18:21:32 +0900

    10.25

---
+++ src/Test/Solution.java
@@ -0,0 +1,23 @@
+package Test;
+
+class Solution {
+
+    static int f(int a){
+        int cnt = 0;
+        while(true){
+            if(a%2==1)
+                cnt++;
+            a/=2;
+            if(a%2==0&&a/2==0)
+                break;
+        }
+        return cnt;
+    }
+
+    public int solution(int n) {
+        int cnt = f(n);
+        for(int i = n+1; ; i++)
+            if(cnt==f(i))
+                return i;
+    }
+}
